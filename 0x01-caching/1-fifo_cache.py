0ff3384d2ee5ce4452df58b6c88674708ac90767
kilokomuli <kilokoemma175@gmail.com>
2024-07-23 07:20:15 -0700

    implement fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -14,15 +14,7 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """Adds an item in the cache"""
         if key is not None and item is not None:
-            if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                first_key = self.queue.pop(0)
-                del self.cache_data[first_key]
-                print("DISCARD: {}".format(first_key))
-                self.cache_data[key] = item
-                if key not in self.queue:
-                    self.queue.append(key)
 
-    def get(self, key):
         """Gets an item from cache"""
         if key is None or key not in self.cache_data:
             return None
