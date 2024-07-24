106e052c0f4ea9472640fabf9d880d1b8d0a3940
kilokomuli <kilokoemma175@gmail.com>
2024-07-24 15:41:55 +0800

    task3 and task 4

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 """LRU Caching module"""
 from base_caching import BaseCaching
+from collections import OrderedDict  
 
+
+class LRUCache(BaseCaching):
+    """ Defines LRU Caching class"""
+    def __init__(self):
+        """Initializes the class"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds item at the end of the cache"""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru, _ = self.cache_data.popitem(last=True)
+                print("DISCARD:", lru)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Gets an item by key"""
+        return self.cache_data.get(key, None)
---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Implement MRU caching"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """Class MRUCache"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                most_recent_key = next(reversed(self.cache_data))
+                del self.cache_data[most_recent_key]
+                print("DISCARD:", most_recent_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
+
+    def get(self, key):
+        """Get an item by key"""
+        return self.cache_data.get(key)
