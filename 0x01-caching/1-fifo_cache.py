2991f8ce54aac8ce03fd3a6779e02f40c0d924b0
kilokomuli <kilokoemma175@gmail.com>
2024-07-23 23:10:30 -0700

    fix

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
-"""FIFO Cache module"""
-from base_caching import BaseCaching
+"""First-In First-Out caching module.
+"""
 from collections import OrderedDict
+
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """Defines and implements FIFO caching system
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal mechanism when the limit is reached.
     """
     def __init__(self):
-        """Initializes the FIFO caching system"""
+        """Initializes the cache.
+        """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Adds an item in the cache"""
-        if key is None and item is None:
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
             return
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
@@ -22,5 +28,6 @@ class FIFOCache(BaseCaching):
             print("DISCARD:", first_key)
 
     def get(self, key):
-        """Gets an item from cache"""
+        """Retrieves an item by key.
+        """
         return self.cache_data.get(key, None)
