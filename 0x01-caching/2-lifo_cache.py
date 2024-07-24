7def4440a2c8ab01f5a6a145f3badf09a2decf97
kilokomuli <kilokoemma175@gmail.com>
2024-07-23 23:17:32 -0700

    update

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -12,7 +12,8 @@ class LIFOCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        if key is None and item is None:
+        """Add an item in the cache"""
+        if key is None or item is None:
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
