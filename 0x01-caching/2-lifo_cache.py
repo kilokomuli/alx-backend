bd947ad05d53dba8d8415161d2cb861369cb73d5
kilokomuli <kilokoemma175@gmail.com>
2024-07-23 23:19:51 -0700

    update fix

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -17,8 +17,8 @@ class LIFOCache(BaseCaching):
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last = self.cache_data.popitem(True)
-                print("DISCARD: {}".format(last))
+                l_key = self.cache_data.popitem(True)
+                print("DISCAR:", l_key)
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
 
