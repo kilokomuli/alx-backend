97675aee98c8bc3f4b380e9294ab6fe52dabf2d7
kilokomuli <kilokoemma175@gmail.com>
2024-07-30 21:33:25 +0300

    locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Get locale from request"""
+from flask import Flask, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """Use request.accept_languages to determine
+    the best match with supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+   """default route"""
+   return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
