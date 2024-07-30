37f5d7f057500705ae342e9e03c45798f776a060
kilokomuli <kilokoemma175@gmail.com>
2024-07-30 19:04:09 +0000

    fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,11 +6,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route("/", strict_slashes=False)
+def hello() -> str:
     """default route"""
     return render_template("0-index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=TRUE)
+    app.run()
