a8eaf68a7958397bf3de7de27f142e5ef4d332f8
kilokomuli <kilokoemma175@gmail.com>
2024-07-30 19:24:02 +0000

    error fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,11 +6,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
+@app.route('/')
+def index():
     """BAsic template render"""
-     return render_template('0-index.html')
+    return render_template("0-index.html",)
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
