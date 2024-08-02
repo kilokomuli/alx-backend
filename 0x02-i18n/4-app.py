71aa4257e337dfd937874adccc09704a37e02b04
kilokomuli <kilokoemma175@gmail.com>
2024-08-02 04:29:41 +0000

    task4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
-"""Parametrize templates"""
-from flask import Flask, render_template
+#!usr/bin/env python3
+"""Force locale with URL parameter"""
+from flask import Flask, request, render_template
 from flask_babel import Babel
 
 
@@ -21,14 +21,18 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """locale"""
+    """Use request.accept_languages to determine
+    the best match with supported languages"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index() -> str:
     """default route"""
-    return render_template("3-index.html")
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
